904a1b4de1ca27af8f0a5e926dda26b23cad946e
PranavMehrotra <87844730+PranavMehrotra@users.noreply.github.com>
2023-01-21 17:08:01 +0530

    Squashed commit of the following:
    commit a06c139a70f4cc693c8483b62148ca2b31beb4d1
    Author: PranavMehrotra <87844730+PranavMehrotra@users.noreply.github.com>
    Date:   Sat Jan 21 17:07:40 2023 +0530
    
        Minor change
    
    commit 37dc050d7496b285f98026818d9601835a3b68d7
    Author: PranavMehrotra <87844730+PranavMehrotra@users.noreply.github.com>
    Date:   Sat Jan 21 16:23:45 2023 +0530
    
        minor changes
    
    commit 05a11d1eae978c984137be860fdb3e0d1f0a8771
    Author: PranavMehrotra <87844730+PranavMehrotra@users.noreply.github.com>
    Date:   Sat Jan 21 12:19:44 2023 +0530
    
        Many optimisations and changes
    
    commit 3a4ee66ae8dd22c3ff245503b9c0ac40b0c6bd8a
    Author: PranavMehrotra <87844730+PranavMehrotra@users.noreply.github.com>
    Date:   Fri Jan 20 19:16:01 2023 +0530
    
        Q 4 and Q7 heavily optimised
    
    commit f01ab44309821e0f20ce5ed122fdf556a0f156b3
    Merge: e5261e0 1a33fc1
    Author: PranavMehrotra <87844730+PranavMehrotra@users.noreply.github.com>
    Date:   Fri Jan 20 18:04:48 2023 +0530
    
        Merge branch 'Pranav' of https://github.com/PranavMehrotra/OS-Lab-23 into Pranav
    
    commit e5261e08b1e27c3ae0f2adaf6c034e044ce4924c
    Author: PranavMehrotra <87844730+PranavMehrotra@users.noreply.github.com>
    Date:   Fri Jan 20 18:04:46 2023 +0530
    
        Trying sed
    
    commit 1a33fc115149f5ac889f5e20e6762aefa9cc41b2
    Author: Saransh Sharma <saransh03sharma@gmail.com>
    Date:   Fri Jan 20 17:31:43 2023 +0530
    
        Q9
    
    commit d8232ed3ef7126df025657b996e86836b04c835a
    Author: Saransh Sharma <saransh03sharma@gmail.com>
    Date:   Fri Jan 20 12:48:19 2023 +0530
    
        Q4 Q7
    
    commit 3dc2eeaf5fdf4c79167262e5bc90305c32efcc25
    Author: Saransh Sharma <saransh03sharma@gmail.com>
    Date:   Thu Jan 19 22:19:25 2023 +0530
    
        Create Q1_newline handled.sh
    
    commit 5129d129009c3e1e6da462ce6b7359de7b76f772
    Author: PranavMehrotra <87844730+PranavMehrotra@users.noreply.github.com>
    Date:   Wed Jan 18 18:35:52 2023 +0530
    
        Q3 and Q6 done
    
    commit da33c63514090bf9703c3eeeb11a81716b52a242
    Author: PranavMehrotra <87844730+PranavMehrotra@users.noreply.github.com>
    Date:   Wed Jan 18 00:19:44 2023 +0530
    
        Q6 completed
    
    commit cb60d737e6f047af9416bac548ec4725aa1c6d27
    Author: Saransh Sharma <saransh03sharma@gmail.com>
    Date:   Tue Jan 17 12:38:17 2023 +0530
    
        Q2
    
    commit fe69c2d8eee101f85076b57755c34a7473beb028
    Author: PranavMehrotra <87844730+PranavMehrotra@users.noreply.github.com>
    Date:   Tue Jan 17 01:01:21 2023 +0530
    
        Many optimisations in Q1 and starting Q3
    
    commit 596bde9a000d51a574b77d9276be8fc67617bd02
    Author: Saransh Sharma <saransh03sharma@gmail.com>
    Date:   Mon Jan 16 16:41:20 2023 +0530
    
        Update Q1.sh
    
    commit aea3c18f5bdc72e6e307bbb0437639a9e6cba50f
    Author: Saransh Sharma <saransh03sharma@gmail.com>
    Date:   Mon Jan 16 09:08:24 2023 +0530
    
        Create Q1.sh
    
    commit 7d72a8317a6e0255d89dff615613ea6d07f4ba76
    Author: PranavMehrotra <87844730+PranavMehrotra@users.noreply.github.com>
    Date:   Sun Jan 15 13:42:59 2023 +0530
    
        Created test

--- Assignments/A1/Q1/Q1_newline handled.sh
+++ Assignments/A1/Q1/Q1_newline handled.sh
@@ -1,2 +1,2 @@
-rev $1 > "c.txt" 
+rev $1 > "b.txt" 
 echo $(gawk -F'\n' '{if(NF > 0){if(NR==1)lcm=$1;else{if($1!=0)lcm=(lcm*$1)/gcd(lcm,$1);}}}function gcd(a,b){return(b==0)?a:gcd(b,a%b);}END{print(lcm);}' "c.txt") | bc
