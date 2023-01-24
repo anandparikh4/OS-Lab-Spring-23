4fec72fa7728ca283648ef67ae947f7cc7b35e7b
PranavMehrotra <87844730+PranavMehrotra@users.noreply.github.com>
2023-01-25 00:09:01 +0530

    Final Submission

--- Assignments/A1/Q1/Assgn1_1_33.sh
+++ Assignments/A1/Q1/Assgn1_1_33.sh
@@ -1,1 +1,1 @@
-gawk -F'\n' '{if(NF > 0){lcm=(NR<2)?$1:(lcm*$1)/gcd(lcm,$1)}}function gcd(a,b){return(b)?gcd(b,a%b):a}END{print lcm}' <(rev "$1")|bc
+rev $1 | gawk -F'\n' '{if(NF) lcm=(NR<2)?$1:(lcm*$1)/gcd(lcm,$1)} function gcd(a,b){return(b)?gcd(b,a%b):a} END{print lcm}' | bc
