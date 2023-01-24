fb8fa4b0fbd34ac412fbd1bd58f68533e5baf5c3
Saransh Sharma <saransh03sharma@gmail.com>
2023-01-24 19:48:25 +0530

    Update Assgn1_9_33.sh

--- Assignments/A1/Q9/Assgn1_9_33.sh
+++ Assignments/A1/Q9/Assgn1_9_33.sh
@@ -1,3 +1,4 @@
-awk '{print $2}' $1 | sort | uniq -c | awk '{print $2" "$1}' | sort -k3 -k2nr
+awk '{print $2}' $1 | sort | uniq -c | awk '{print $2" "$1}' | sort -k3 -k2nr 
+echo ""
 awk '{print $1}' $1 | sort | uniq -d
 awk '{print $1}' $1 | sort | uniq -u|wc -l
