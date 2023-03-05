b05c1896ea41b758fa705bd403fdcccba12b6379
anandparikh4 <98024272+anandparikh4@users.noreply.github.com>
2023-03-05 12:43:47 +0530

    default destructors added

--- Assignments/A4/defs.cpp
+++ Assignments/A4/defs.cpp
@@ -12,9 +12,46 @@ void exit_with_error(string s){
     exit(0);
 }
 
+// Class Action 
+
+Action::Action():
+user_id(0), action_id(0), timestamp(0), action_type(0)
+{}
+
+Action::Action(int uid, int aid, int times, int action_t):
+user_id(uid), action_id(aid), timestamp(times), action_type(action_t)
+{}
+
+Action::Action(const Action &a):
+user_id(a.user_id), action_id(a.action_id), timestamp(a.timestamp), action_type(a.action_type) 
+{}
+
+Action::~Action()
+{}
+
 void Action::print() {
     cout << "User ID: " << user_id << " Action ID: " << action_id << " Timestamp: " << timestamp << " Action Type: " << action_type << std::endl;
 }
+
+// Class Node
+
+Node::Node(): 
+user_id(0), degree(0), num_actions(0), priority(0), log_degree(0)
+{}
+
+Node::Node(int uid, int deg, int actions, int pri): 
+user_id(uid), degree(deg), num_actions(actions), priority(pri) 
+{
+    if(degree == 0) log_degree = 0;
+    else log_degree = log2(degree);
+}
+
+Node::Node(const Node &u):
+user_id(u.user_id), degree(u.degree), num_actions(u.num_actions), priority(u.priority), log_degree(u.log_degree) 
+{}
+
+Node::~Node()
+{}
 
 void Node::print(){
     cout << "User ID: " << user_id << " Degree: " << degree << " Log Degree: " << log_degree << " Num Actions: " << num_actions << " Priority: " << priority << std::endl;
--- Assignments/A4/defs.h
+++ Assignments/A4/defs.h
@@ -3,7 +3,6 @@
 
 #include <vector>
 #include <string>
-#include <cmath>
 
 void exit_with_error(std::string);
 
@@ -15,15 +14,11 @@ class Action{
         long timestamp;
         int action_type;
 
-        Action(): 
-            user_id(0), action_id(0), timestamp(0), action_type(0)
-            {}
-        Action(int uid, int aid, int times, int action_t): 
-            user_id(uid), action_id(aid), timestamp(times), action_type(action_t)
-            {}
-        Action(const Action &a): 
-            user_id(a.user_id), action_id(a.action_id), timestamp(a.timestamp), action_type(a.action_type) 
-            {}
+        Action();
+        Action(int uid, int aid, int times, int action_t);
+        Action(const Action &a);
+
+        ~Action();
 
         void print();
 };
@@ -39,20 +34,11 @@ class Node{
         std::vector<Action> wall;
         std::vector<Action> feed;
         
-        Node(): 
-            user_id(0), degree(0), num_actions(0), priority(0), log_degree(0)
-            {}
+        Node();
+        Node(int uid, int deg=0, int actions=0, int pri=0);
+        Node(const Node &u);
 
-        Node(int uid, int deg=0, int actions=0, int pri=0): 
-            user_id(uid), degree(deg), num_actions(actions), priority(pri) 
-            {
-                if(degree == 0) log_degree = 0;
-                else log_degree = log2(degree);
-            }
-
-        Node(const Node &u):
-            user_id(u.user_id), degree(u.degree), num_actions(u.num_actions), priority(u.priority), log_degree(u.log_degree) 
-            {}
+        ~Node();
         
         void print();
 };
